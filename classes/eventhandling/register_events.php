c2383e58f5e61a766d029d8216c9c9ed56ebaee2
Justus Dieckmann <justusdieckmann@wwu.de>
2019-01-29 18:16:45 +0100

    init

---
+++ classes/eventhandling/register_events.php
@@ -0,0 +1,51 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *
+ *
+ * @package    report_activity_analysis
+ * @copyright  2019 Justus Dieckmann <justusdieckmann@wwu.de>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace report_activity_analysis\eventhandling;
+
+class register_events {
+
+    public static function get_event_observers() {
+        $observers = [];
+
+        $pluginman = \core_plugin_manager::instance();
+        foreach ($pluginman->get_plugins() as $type => $plugins) {
+            foreach ($plugins as $plugin) {
+                $name = $plugin->type . "_" . $plugin->name;
+                switch ($name) {
+                    case "mod_resource":
+                        $observers[] = array(
+                                //"event" => "\\$name\\event\\course_module_viewed",
+                                "event" => "\\mod_resource\\event\\course_module_viewed",
+                                "callback" => '\\report_activity_analysis\\handler::handle'
+                        );
+                        break;
+                }
+            }
+        }
+
+        return $observers;
+    }
+
+}
